c694b3f1f012b46c59b2f1bdf27837499691dfca
Damien Pontifex <damien.pontifex@gmail.com>
2019-07-30 09:01:33 +1000

    preprocessing serving to accept jpeg raw bytes

--- Serving.Dockerfile
+++ Serving.Dockerfile
@@ -1,8 +1,6 @@
-ARG MODEL_VERSION=1
-
 FROM tensorflow/serving
 
 ENV MODEL_BASE_PATH /models
 ENV MODEL_NAME catdog
 
-COPY export/mobilenet_finetuned /models/catdog/$MODEL_VERSION
+COPY export/transformed_for_serving /models/catdog/1
--- binary_classifier_train.py
+++ binary_classifier_train.py
@@ -6,6 +6,7 @@ import pickle
 from argparse import ArgumentParser
 import logging
 
+import numpy as np
 import tensorflow as tf
 import tensorflow_hub as hub
 import tensorflowjs as tfjs
@@ -70,7 +71,6 @@ def make_dataset(path, batch_size=32):
     )
     return ds
 
-
 #%%
 mobilenet_url = 'https://tfhub.dev/google/tf2-preview/mobilenet_v2/feature_vector/4'
 
@@ -124,3 +124,49 @@ tf.saved_model.save(model, 'export/mobilenet_finetuned')
 
 #%%
 tfjs.converters.convert_tf_saved_model('export/mobilenet_finetuned', 'export/web_model')
+
+#%%
+@tf.function(input_signature=[tf.TensorSpec(shape=[None], dtype=tf.string)])
+def serving(input_image):
+
+    # Convert bytes of jpeg input to float32 tensor for model
+    def _input_to_feature(img):
+        img = tf.io.decode_jpeg(img, channels=3)
+        img = tf.image.convert_image_dtype(img, tf.float32)
+        img = tf.image.resize_with_pad(img, 224, 224)
+        return img
+    img = tf.map_fn(_input_to_feature, input_image, dtype=tf.float32)
+
+    # Predict
+    predictions = model(img)
+
+    with open('export/idx2class.pkl', 'rb') as f:
+        class_names = pickle.load(f)
+        class_names = tf.constant(class_names, dtype=tf.string)
+
+    # Single output for model so collapse final axis for vector output
+    predictions = tf.squeeze(predictions, axis=-1)
+
+    # Predictions are output from sigmoid so float32 in range 0 -> 1
+    # Round to integers for predicted class and string lookup for class name
+    prediction_integers = tf.cast(tf.math.round(predictions), tf.int32)
+    predicted_classes = tf.map_fn(lambda idx: class_names[idx], prediction_integers, dtype=tf.string)
+
+    # Convert sigmoid output for probability
+    # 1 (dog) will remain at logit output
+    # 0 (cat) will be 1.0 - logit to give probability
+    def to_probability(logit):
+        if logit < 0.5:
+            return 1.0 - logit
+        else:
+            return logit
+    class_probability = tf.map_fn(to_probability, predictions, dtype=tf.float32)
+
+    return {
+        'classes': predicted_classes,
+        'probabilities': class_probability
+    }
+
+tf.saved_model.save(model, export_dir='export/transformed_for_serving', signatures=serving)
+
+#%%
